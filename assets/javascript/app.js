ec5772fd23716aa7488b2b1de8f5d9801f4934db
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-02-27 23:24:23 -0600

    updated bare HTML and created TODO item in JS file.

---
+++ assets/javascript/app.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    var questions = [];
+
+    // TODO: Use an array to store 10 questions, and for each questions have 4 answers. Each question will have a number identifier, and each answer will have a number identifier.
+});
