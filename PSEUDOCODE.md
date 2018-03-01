fbf2cfd752aa1b733309f4d1eddd8aed6d2538bb
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-01 08:23:22 -0600

    modified app.js to include function in the object declaration; updated tasks in PSEUDOCODE.md

--- PSEUDOCODE.md
+++ PSEUDOCODE.md
@@ -9,10 +9,25 @@
 
 
 ### TODO
-- [ ] Store questions to be displayed
-- [ ] Store answers for each question
+- [X] Store questions to be displayed
+- [X] Store answers for each question
 - [ ] Remove question/answer combo to be chosen again
 - [ ] Display timer when new question is displayed
 - [ ] Show answer when question has been guessed/time runs out
 - [ ] Display correct/incorrect answers when game is over
 - [ ] Clear screen and restart game when "Restart" button is clicked
+
+
+### Pseudocode
+
+##### Timer Function
+* After "Start" button has been clicked, display timer
+    - Once timer is displayed, start countdown
+    - If the timer reached 00:00, display correct answer
+        - Display correct answer for 5 seconds
+
+
+##### Questions Functionality
+* Use an Object to store questions with possible answers
+* Create an index number that will randomly select a question from the Object list of questions
+
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,41 +3,42 @@ $(document).ready(function () {
     // Object to hold questions and answers
     var questions = [
         {
-            value: 0,
             image: "../images/Cowboys.png",
             question: "Who is the current quarterback for this team?",
             answers: ["Tony Romo", "Dak Prescott", "Rush Cooper", "Danny Manziel"]
         },
         {
-            value: 1,
             image: "../images/Bears.png",
             question: "In what city is this team located?",
             answers: ["Peoria", "Joilet", "Rockford", "Chicago"]
         },
         {
-            value: 2,
             image: "../images/Bills.png",
             question: "What famous food originated in this team's city?",
             answers: ["Buffalo Wings", "Philly Cheesesteak", "Deep Dish Pizza", "Cheesecake"]
         },
         {
-            value: 3,
             image: "../images/Broncos.png",
             question: "Which former QB of this team enjoys Chicken Parmesan?",
             answers: ["John Elway", "Tim Tebow", "Peyton Manning", "Brock Osweiler"]
         },
         {
-            value: 4,
             image: "../images/Browns.png",
             question: "Select the NBA team that shares this team city. Hint: they won the 2016 NBA Championship Title",
             answers: ["Boston Celtics", "Golden State Warriors", "Miami Heat", "Cleveland Cavaliers"]
         },
         {
-            value: 5,
             image: "../images/Falcons.png",
             question: "This team had a dance back in 1998 called the \"Dirty Bird\", where is team located?",
             answers: ["Atlanta", "Seattle", "Philadelphia", "Arizona"]
         },
+
+        function displayAnswers(question) {
+            for (var i = 0; i < answers; i++) {
+                // Enter code here to display the answers whenever a question is chosen
+                $("#content").append().html("<p id=\"answers\">" + questions.displayAnswers(question) + "</p>");
+            }
+        }
     ];
 
     // Used to select question at random index in array
@@ -46,7 +47,9 @@ $(document).ready(function () {
     $("#start").click(function () {
         $("#content").append().html('<p id="question"></p>');
         var question = $("#question");
-        question.text(questions[0].question);
+        console.log(index);
+        console.log(questions.length);
+        question.text(questions[index].question);
     });
 
 
