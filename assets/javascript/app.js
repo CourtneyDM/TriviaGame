0981496603b0cf16cf31d17ba568bb9b8dcdd055
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-02 07:53:39 -0600

    created logic to check the choice selected, added a gif file for results testing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,56 +1,102 @@
 $(document).ready(function () {
-
-    // Object to hold questions and answers
+    var content = $("#content");
+    // Object to hold questions and choices
     var questions = [
         {
             image: "../images/Cowboys.png",
             question: "Who is the current quarterback for this team?",
-            answers: ["Tony Romo", "Dak Prescott", "Rush Cooper", "Danny Manziel"]
+            choices: ["Tony Romo", "Dak Prescott", "Rush Cooper", "Danny Manziel"],
+            answer: "Dak Prescott",
+            asked: false
         },
         {
             image: "../images/Bears.png",
             question: "In what city is this team located?",
-            answers: ["Peoria", "Joilet", "Rockford", "Chicago"]
+            choices: ["Peoria", "Joilet", "Rockford", "Chicago"],
+            answer: "Chicago",
+            asked: false
         },
         {
             image: "../images/Bills.png",
             question: "What famous food originated in this team's city?",
-            answers: ["Buffalo Wings", "Philly Cheesesteak", "Deep Dish Pizza", "Cheesecake"]
+            choices: ["Buffalo Wings", "Philly Cheesesteak", "Deep Dish Pizza", "Cheesecake"],
+            answer: "Buffalo Wings",
+            asked: false
         },
         {
             image: "../images/Broncos.png",
             question: "Which former QB of this team enjoys Chicken Parmesan?",
-            answers: ["John Elway", "Tim Tebow", "Peyton Manning", "Brock Osweiler"]
+            choices: ["John Elway", "Tim Tebow", "Peyton Manning", "Brock Osweiler"],
+            answer: "Peyton Manning",
+            asked: false
         },
         {
             image: "../images/Browns.png",
             question: "Select the NBA team that shares this team city. Hint: they won the 2016 NBA Championship Title",
-            answers: ["Boston Celtics", "Golden State Warriors", "Miami Heat", "Cleveland Cavaliers"]
+            choices: ["Boston Celtics", "Golden State Warriors", "Miami Heat", "Cleveland Cavaliers"],
+            answer: "Cleveland Cavaliers",
+            asked: false
         },
         {
             image: "../images/Falcons.png",
             question: "This team had a dance back in 1998 called the \"Dirty Bird\", where is team located?",
-            answers: ["Atlanta", "Seattle", "Philadelphia", "Arizona"]
+            choices: ["Atlanta", "Seattle", "Philadelphia", "Arizona"],
+            answer: "Atlanta",
+            asked: false
         },
-
-        function displayAnswers(question) {
-            for (var i = 0; i < answers; i++) {
-                // Enter code here to display the answers whenever a question is chosen
-                $("#content").append().html("<p id=\"answers\">" + questions.displayAnswers(question) + "</p>");
-            }
-        }
     ];
 
-    // Used to select question at random index in array
+
+    $("#start").click(showQuestion);
     var index = Math.floor(Math.random() * questions.length);
 
-    $("#start").click(function () {
-        $("#content").append().html('<p id="question"></p>');
-        var question = $("#question");
-        console.log(index);
-        console.log(questions.length);
-        question.text(questions[index].question);
-    });
+
+    function showQuestion() {
+        content.empty();
+        $("button").off();
+
+        $("button").removeAttr("id");
+        $("button").text("Submit Answers");
+        $("button").attr("id", "submit");
+
+        content.append("<div id='timer'></div>");
+        var question = questions[index].question;
+        var asked = questions[index].asked;
+
+        content.append(question);
+        asked = true;
+        showAnswers();
+    }
+
+    function showAnswers() {
+        var answers = questions[index].choices;
+        var correct = questions[index].answer;
+
+        console.log(answers);
+        console.log(correct);
+
+        for (var i = 0; i < answers.length; i++) {
+            var answer = answers[i];
+            $("#answers").append("<input type='radio' name='answer' value='" + answer + "'/>" + answer);
+        }
 
 
+        $("#submit").click(function () {
+            var chosen = $("input:checked").val();
+            if (!chosen) {
+                console.log("no answer selected");
+            }
+            else
+                if (chosen === correct) {
+                    // console.log("Correct answer");
+                    $("button").hide();
+                    $("#answers").append("<img src='https://media3.giphy.com/media/l8HFdR4jE4SM8/200w.webp'/>")
+                }
+                else {
+                    $("button").hide();
+                    $("#answers").append("<img src='https://media3.giphy.com/media/yhWfYEaF7TkQ0/giphy.gif'/>")
+                }
+
+        });
+    }
 });
