9f20d64bc7703d3effb64da42b6917a8c9a915fc
Courtney Montgomery <courtdmontgomery@gmail.com>
2018-03-03 17:44:55 -0600

    updated html to include section for questions and answers, updated timer logic

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,7 @@
 $(document).ready(function () {
-    var content = $("#content");
+    var answerDisplay = $("#answers");
+    var questionDisplay = $("#questions");
+
     // Object to hold questions and choices
     var questions = [
         {
@@ -46,64 +48,130 @@ $(document).ready(function () {
         },
     ];
 
-    var intervalId;
-    var timerRunning = false;
+    var counter = 0;
+    var correct_answers = 0;
+    var incorrect_answers = 0;
+    var intervalID;
+    var index;
 
+    // Timer Object
+    var timer = {
+        time_remaining: 0,
 
+        decrementTimer: function () {
+            timer.time_remaining--;
+            if (timer.time_remaining < 10) {
+                $("#timer").html("<p>00:0" + timer.time_remaining + "</p>");
+            }
+            else {
+                $("#timer").html("<p>00:" + timer.time_remaining + "</p>");
+            }
+            if (timer.time_remaining === 0) {
+                timer.stopTimer();
+                alert("Time has expired");
+                clearContent();
+            }
+        },
 
+        startTimer: function () {
+            intervalID = setInterval(timer.decrementTimer, 1000);
+        },
+
+        stopTimer: function () {
+            clearInterval(intervalID)
+        }
+    }
+
+    // Start game when button has been clicked
     $("#start").click(showQuestion);
-    var index = Math.floor(Math.random() * questions.length);
 
-
+    // Display questions to screen
     function showQuestion() {
+        index = Math.floor(Math.random() * questions.length);
 
-        content.empty();
+        var question = questions[index].question;
+        var asked = questions[index].asked;
+
+        console.log("counter: " + counter);
+        console.log(asked);
+
+        if (asked === true) {
+            counter++;
+            if (counter === questions.length) {
+                // create a function to show the results
+                console.log(counter + " questions have been asked.");
+            }
+            else {
+                index++;
+                console.log(counter);
+            }
+        }
+
+        $("#questions").empty();
+        timer.time_remaining = 5;
+        timer.startTimer();
+
+        $("#instructions").hide();
         $("button").off();
 
         $("button").removeAttr("id");
         $("button").text("Submit Answers");
         $("button").attr("id", "submit");
 
-        content.append("<div id='timer'></div>");
-        var question = questions[index].question;
-        var asked = questions[index].asked;
 
-        content.append(question);
-        asked = true;
+        $("#questions").html("<p>" + question + "</p>");
+
         showAnswers();
+        questions[index].asked = true;
     }
 
+    // Display answers to screen
     function showAnswers() {
         var answers = questions[index].choices;
         var correct = questions[index].answer;
-
-        console.log(answers);
-        console.log(correct);
 
         for (var i = 0; i < answers.length; i++) {
             var answer = answers[i];
             $("#answers").append("<input type='radio' name='answer' value='" + answer + "'/>" + answer);
         }
-
-
-        $("#submit").click(function () {
-            var chosen = $("input:checked").val();
-            if (!chosen) {
-                console.log("no answer selected");
-            }
-            else
-                if (chosen === correct) {
-                    // console.log("Correct answer");
-                    $("button").hide();
-                    $("#answers").append("<img src='https://media3.giphy.com/media/l8HFdR4jE4SM8/200w.webp'/>")
-                }
-                else {
-                    $("button").hide();
-                    $("#answers").append("<img src='https://media3.giphy.com/media/yhWfYEaF7TkQ0/giphy.gif'/>")
-                }
-
-        });
+        $("#submit").on("click", checkAnswer);
     }
 
+    // Check answers
+    function checkAnswer() {
+        var correct = questions[index].answer;
+        var chosen = $("input:checked").val();
 
+        if (!chosen) {
+            alert("no answer selected");
+        }
+        else
+            if (chosen === correct) {
+                // console.log("Correct answer");
+                $("button").hide();
+                timer.stopTimer();
+                alert("Correct.");
+                // $("#answers").append("<img src='https://media3.giphy.com/media/l8HFdR4jE4SM8/200w.webp'/>");
+                clearInterval(timer.intervalID);
+                clearContent();
+            }
+            else {
+                $("button").hide();
+                alert("Incorrect.");
+                timer.stopTimer();
+                // $("#answers").append("<img src='https://media3.giphy.com/media/yhWfYEaF7TkQ0/giphy.gif'/>");
+                clearInterval(timer.intervalID);
+                clearContent();
+            }
+    }
+
+    // Clear content
+    function clearContent() {
+        questionDisplay.empty();
+        answerDisplay.empty();
+        // clearInterval(timer.intervalID);
+        $("button").css("display", "inherit");
+        $("#timer").html("<p>00:20</p>");
+        showQuestion();
+    }
 });
